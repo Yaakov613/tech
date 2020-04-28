e5efcbda2b2dd0e11c47040f4cd1d416dea4eba2
Yaakov613 <61311180+Yaakov613@users.noreply.github.com>
2020-04-28 16:43:12 +0200

    nice new fetures

--- src/App.js
+++ src/App.js
@@ -10,8 +10,47 @@ class App extends Component {
       [null, null, null],
       [null, null, null]
     ],
-    turn: 'X'
+    turn: 'X',
+    count: 0
   }
+  resfreshState = () => {
+    this.setState({
+      boxes: [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+      ],
+      turn: 'X',
+      count: 0
+    })
+  }
+
+  checkforWinner = (updateDatedRows) => {
+    if ((updateDatedRows[0][0] !== null && updateDatedRows[0][0] === updateDatedRows[1][1] && updateDatedRows[2][2]) ||
+      (updateDatedRows[0][2] !== null && updateDatedRows[0][2] === updateDatedRows[1][1] && updateDatedRows[2][0])) {
+      setTimeout(() => {
+        alert('we have a winner')
+        this.resfreshState()
+      }, 500);
+    }
+    if ((updateDatedRows[0][0] !== null && updateDatedRows[0][0] === updateDatedRows[1][0] && updateDatedRows[2][0]) ||
+      (updateDatedRows[0][1] !== null && updateDatedRows[0][1] === updateDatedRows[1][1] && updateDatedRows[2][1]) ||
+      (updateDatedRows[0][2] !== null && updateDatedRows[0][2] === updateDatedRows[1][2] && updateDatedRows[2][2])) {
+      setTimeout(() => {
+        alert('we have a winner')
+        this.resfreshState()
+      }, 500);
+    }
+    if ((updateDatedRows[0][0] !== null && updateDatedRows[0][0] === updateDatedRows[0][1] && updateDatedRows[0][2]) ||
+      (updateDatedRows[1][0] !== null && updateDatedRows[1][0] === updateDatedRows[1][1] && updateDatedRows[1][2]) ||
+      (updateDatedRows[2][0] !== null && updateDatedRows[2][0] === updateDatedRows[2][1] && updateDatedRows[2][2])) {
+      setTimeout(() => {
+        alert('we have a winner!!')
+        this.resfreshState()
+      }, 1000);
+    }
+  }
+
   checkBoxHandler = (index, box) => {
     if (!box) {
       const updatedFirstRow = [...this.state.boxes[0]]
@@ -19,11 +58,21 @@ class App extends Component {
       const updatedBoxes = [updatedFirstRow, this.state.boxes[1], this.state.boxes[2]]
       let turn = 'X'
       if (this.state.turn === 'X') { turn = '0' }
+      const winnerble = updatedBoxes.map((row, index) => {
+        const filteredRow = row.filter(item => item === null)
+        return filteredRow.length > 1
+      })
+      console.log(this.state.count)
+      console.log(winnerble)
+      const newCount = this.state.count + 1
+      if (newCount > 4) { this.checkforWinner(updatedBoxes) }
       this.setState({
         boxes: updatedBoxes,
-        turn: turn
+        turn: turn,
+        count: newCount
       })
     }
+
   }
   checkBoxHandler1 = (index, box) => {
     if (!box) {
@@ -32,7 +81,13 @@ class App extends Component {
       const updatedBoxes = [this.state.boxes[0], updatedSecondRow, this.state.boxes[2]]
       let turn = 'X'
       if (this.state.turn === 'X') { turn = '0' }
-      this.setState({ boxes: updatedBoxes, turn, })
+      const newCount = this.state.count + 1
+      if (newCount > 4) { this.checkforWinner(updatedBoxes) }
+      this.setState({
+        boxes: updatedBoxes,
+        turn,
+        count: newCount
+      })
     }
   }
 
@@ -43,9 +98,12 @@ class App extends Component {
       const updatedBoxes = [this.state.boxes[0], this.state.boxes[1], updatedThirdRow]
       let turn = 'X'
       if (this.state.turn === 'X') { turn = '0' }
+      const newCount = this.state.count + 1
+      if (newCount > 4) { this.checkforWinner(updatedBoxes) }
       this.setState({
         boxes: updatedBoxes,
         turn,
+        count: newCount
       })
     }
   }
@@ -53,17 +111,14 @@ class App extends Component {
 
   render() {
     const firstRow = this.state.boxes[0].map((box, index) => { return <Sum clicked={() => this.checkBoxHandler(index, box)} key={index} type={box} /> })
-    const secondRow = this.state.boxes[1].map((box, index) => { return <Sum clicked={() => this.checkBoxHandler1(index,box)} key={index} type={box} /> })
-    const thirdRow = this.state.boxes[2].map((box, index) => { return <Sum clicked={() => this.checkBoxHandler2(index,box)} key={index} type={box} /> })
-    console.log(this.state.boxes)
+    const secondRow = this.state.boxes[1].map((box, index) => { return <Sum clicked={() => this.checkBoxHandler1(index, box)} key={index} type={box} /> })
+    const thirdRow = this.state.boxes[2].map((box, index) => { return <Sum clicked={() => this.checkBoxHandler2(index, box)} key={index} type={box} /> })
     return (
       <div>
         <h1>welocome to my tic tac toe</h1>
         <h3>{this.state.turn} to play</h3>
         <div style={{ textAlign: 'center' }}>{firstRow}</div>
-        <hr />
         <div style={{ textAlign: 'center' }}>{secondRow}</div>
-        <hr />
         <div style={{ textAlign: 'center' }}>{thirdRow}</div>
       </div>
     )
