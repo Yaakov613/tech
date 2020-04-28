b28eb1c451ff08a92af4760138bd401aa5d30093
Yaakov613 <61311180+Yaakov613@users.noreply.github.com>
2020-04-28 11:23:31 +0200

    test commit

---
+++ src/sum.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from '../src/App.module.css'
+
+const sum=(props)=>{
+    return(
+        <div className={classes.TestBox}>
+            <p className={classes.Text}>paddig</p>
+        </div>
+    )
+}
+
+export default sum
